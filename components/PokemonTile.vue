4a2337f339a3d304c90ba4fae3b0acd1887064a3
Alvin Galit <aroblesgalit@gmail.com>
2023-05-28 22:55:51 -0700

    Created component for pokemon tile

---
+++ components/PokemonTile.vue
@@ -0,0 +1,22 @@
+<template>
+    <div 
+        :id="char.id"
+        class="shadow p-4 flex flex-col items-center justify-end gap-4"
+    >
+      <img :src="char.sprite" alt="" />
+      <p>
+        {{ char.num + ' ' + char.species }}
+      </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PokemonTile',
+    props: ['char']
+}
+</script>
+
+<style scoped>
+
+</style>
