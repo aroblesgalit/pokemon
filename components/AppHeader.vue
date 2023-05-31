b8f721982775e064db415b94e8bca33e7aa1bf53
Alvin Galit <aroblesgalit@gmail.com>
2023-05-30 17:59:22 -0700

    Created header and default layout

---
+++ components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="flex justify-between items-center mb-4 pb-4 border border-solid border-slate-400">
+        <img src="" alt="">
+        <ul class="flex">
+            <li>
+                <nuxt-link to="/" class="inline-block py-1 px-4 mr-2">Home</nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="my-8 mx-auto py-4 px-8 overflow-hidden">
+        <AppHeader />
+        <nuxt />
+    </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader.vue'
+
+export default {
+    components: {
+        AppHeader
+    }
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    line-height: 1.6;
+}
+
+a {
+    text-decoration: none;
+}
+
+ul {
+    list-style: none;
+}
+</style>
