2be147fa16c01516c7df7406a8818ac17ed8c1c4
Alvin Galit <aroblesgalit@gmail.com>
2023-06-11 21:10:41 -0700

    Created detail page

--- components/PokemonTile.vue
+++ components/PokemonTile.vue
@@ -1,16 +1,18 @@
 <template>
-    <div 
-        :id="char.id"
-        :class="[bgColor]"
-        class="pokemon-tile relative shadow p-4 flex flex-col items-center justify-end gap-4"
-    >
-        <span class="absolute text-sm font-bold text-white/75">{{ char.num }}</span>
-        <div class="sprite relative w-full">
-            <img :src="char.sprite" alt="" class="relative z-10 mx-auto" />
-            <div class="absolute bottom-0 left-0 right-0 mx-auto text-center bg-white/50 z-0"></div>
+    <nuxt-link :to="`/pokemon/${char.num}`">
+        <div 
+            :id="char.num"
+            :class="[bgColor]"
+            class="pokemon-tile relative shadow p-4 flex flex-col items-center justify-end gap-4"
+        >
+            <span class="absolute text-sm font-bold text-white/75">{{ char.num }}</span>
+            <div class="sprite relative w-full">
+                <img :src="char.sprite" alt="" class="relative z-10 mx-auto" />
+                <div class="absolute bottom-0 left-0 right-0 mx-auto text-center bg-white/50 z-0"></div>
+            </div>
+            <p class="capitalize">{{ charSpecies }}</p>
         </div>
-        <p class="capitalize">{{ charSpecies }}</p>
-    </div>
+    </nuxt-link>
 </template>
 
 <script>
---
+++ pages/pokemon/_id/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        {{ char.species }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            char: {}
+        }
+    },
+    async fetch() {
+        const pokemon = await fetch('https://graphqlpokemon.favware.tech/v7', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          query: `
+            {
+              getPokemonByDexNumber(number: ${this.$route.params.id}) {
+                num
+                species
+                sprite
+                color
+              }
+            }
+          `
+        })
+      }).then(res => res.json())
+        .then(res => JSON.parse(JSON.stringify(res.data.getPokemonByDexNumber)))
+      
+      this.char = pokemon
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
